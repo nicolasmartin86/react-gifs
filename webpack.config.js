481ef64b44edec57f71547ee2a3e8d7f7cf8e4ee
Nicolas Martin <nicolas.artur.martin@gmail.com>
2019-06-20 14:59:58 +0200

    change for webpack config to update with latest versions

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  mode: "development",
   plugins: [
     new HtmlWebpackPlugin({
       template: '!!html-loader!templates/index.html'
